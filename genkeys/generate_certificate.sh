53cb823cc681061eb7f1dc01ab8c440f00d75da4
Kees Trippelvitz <github@nabber.nl>
2007-02-23 15:01:46 +0000

    Fixed paths.
    git-svn-id: https://svn.surfnet.nl/svn/ids/tunnel/branches/1.04beta@208 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- genkeys/generate_certificate.sh
+++ genkeys/generate_certificate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Load the generic certificate variables.
-. /home/surfnetids/tunnel/trunk/genkeys/vars.conf
+. /opt/surfnetids/genkeys/vars.conf
 
 # Client specific variables.
 KEY_NAME=$1
