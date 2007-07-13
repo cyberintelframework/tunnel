2f8ea82a09ec05b90a3a94dc9d7641e50d20a98d
Kees Trippelvitz <github@nabber.nl>
2007-07-13 16:14:38 +0000

    Added IGMP data set.
    git-svn-id: https://svn.surfnet.nl/svn/ids/tunnel/trunk@519 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ scripts/types_igmp.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+%igmptypes = (
+	1 => "Create Group Request",
+	2 => "Create Group Reply",
+	3 => "Join Group Request",
+	4 => "Join Group Reply",
+	5 => "Leave Group Request",
+	6 => "Leave Group Reply",
+	7 => "Confirm Group Request",
+	8 => "Confirm Group Reply",
+);
