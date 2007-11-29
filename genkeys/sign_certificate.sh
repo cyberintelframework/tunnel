8416bb93deaef540b012f4d2caa94fa7905d6d35
Kees Trippelvitz <github@nabber.nl>
2007-11-29 13:29:08 +0000

    Adding stable 2.00 release of the tunnel package.
    git-svn-id: https://svn.surfnet.nl/svn/ids/2.0/tunnel/tags/stable-2.00@735 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ genkeys/sign_certificate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Load the generic certificate variables.
+. /opt/surfnetids/2.0/tunnel/trunk/genkeys/vars.conf
+
+# Client specific variables.
+KEY_NAME=$1
+export KEY_COMMONNAME=$KEY_NAME
+
+# Run the signing request command.
+sh $genkeys/sign-req $clientkeys/$KEY_NAME
