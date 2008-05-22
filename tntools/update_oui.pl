309b8e25ed0c9daf78df9f03ae884c5f004ce4b1
Kees Trippelvitz <github@nabber.nl>
2008-05-22 14:02:14 +0000

    Added unregistered QEMU & Bochs OUI
    git-svn-id: https://svn.surfnet.nl/svn/ids/2.10/tunnel/trunk@777 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- tntools/update_oui.pl
+++ tntools/update_oui.pl
@@ -2,15 +2,16 @@
 
 #########################################
 # Update script for oui.txt             #
-# SURFnet IDS                           #
-# Version 2.00.01                       #
-# 14-09-2007                            #
+# SURFnet IDS 2.10.00                   #
+# Changeset 002                         #
+# 22-05-2007                            #
 # Jan van Lith & Kees Trippelvitz       #
 #########################################
 
 #############################################
 # Changelog:
-# 2.00.01 Initial release.
+# 002 Added QEMU and Bochs OUI
+# 001 Initial release.
 #############################################
 
 ##################
@@ -21,4 +22,6 @@ do '/etc/surfnetids/surfnetids-tn.conf';
 `wget -O $c_surfidsdir/scripts/oui_new.txt http://standards.ieee.org/regauth/oui/oui.txt`;
 `grep "\(hex\)" $c_surfidsdir/scripts/oui_new.txt | awk '{gsub(/-/,":",\$1);\$2="";print}' > $c_surfidsdir/scripts/oui.txt`;
 `rm $c_surfidsdir/scripts/oui_new.txt`;
+`echo "52:54:00  QEMU" >> $c_surfidsdir/scripts/oui.txt`;
+`echo "B0:C4:20  Bochs" >> $c_surfidsdir/scripts/oui.txt`;
 print "Updating oui database done!\n";
