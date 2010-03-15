f66248402dbc537c95f346a86f1ffbfc6738c1b0
Kees Trippelvitz <github@nabber.nl>
2010-03-15 10:59:52 +0000

    Changed script to work with new database layout.
    git-svn-id: https://svn.surfnet.nl/svn/ids/tunnel/trunk@1569 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- tntools/reset_sensors_db.pl
+++ tntools/reset_sensors_db.pl
@@ -25,25 +25,15 @@ use Time::localtime qw(localtime);
 do '/etc/surfnetids/surfnetids-tn.conf';
 require "$c_surfidsdir/scripts/tnfunctions.inc.pl";
 
-connectdb();
+dbconnect();
 
-$sql = "SELECT id, netconf, tap FROM sensors WHERE status = 1";
+$sql = "SELECT sensors.id FROM sensors, sensor_details WHERE sensors.keyname = sensor_details.keyname AND status = 1 AND NOT permanent = 1";
 $sth = $dbh->prepare($sql);
 $er = $sth->execute();
 
 while (@row = $sth->fetchrow_array) {
   $sid = $row[0];
-  $netconf = $row[1];
-  $tap = $row[2];
-
-  if ("$tap" ne "") {
-    if ($netconf eq "dhcp" || $netconf eq "vland") {
-      $sql = "UPDATE sensors SET tap = '', tapip = NULL, status = 0 WHERE tap = '$tap'";
-      $er = $dbh->do($sql);
-    } elsif ($netconf eq "static" || $netconf eq "vlans") {
-      $sql = "UPDATE sensors SET tap = '', status = 0 WHERE tap = '$tap'";
-      $er = $dbh->do($sql);
-    }
-  }
+  $sql = "UPDATE sensors SET tap = '', tapip = NULL, status = 0 WHERE id = '$sid'";
+  $er = $dbh->do($sql);
 }
 
