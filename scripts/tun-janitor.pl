ca1b1d952cc8a6dcd1400ae7411f52a9ee6c8908
Kees Trippelvitz <github@nabber.nl>
2010-12-17 14:23:17 +0000

    Added janitor script for the tunnel server.
    git-svn-id: https://svn.surfnet.nl/svn/ids/tunnel/trunk@1729 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ scripts/tun-janitor.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+##################
+# Includes
+##################
+do '/etc/surfnetids/surfnetids-tn.conf';
+require "$c_surfidsdir/scripts/tnfunctions.inc.pl";
+
+our $source = 'tun-janitor.pl';
+our $sensor = 'unkown';
+our $tap = 'unknown';
+our $remoteip = '0.0.0.0';
+our $pid = $$;
+our $g_vlanid = 0;
+
+##################
+# Modules used
+##################
+use DBI;
+use Time::localtime qw(localtime); 
+
+##################
+# Main script
+##################
+
+$result = dbconnect();
+if ($result eq 'false') {
+    print "No database connection";
+    exit(6);
+}
+
+$sql = "SELECT arp, dhcp, ipv6, protos, keyname, vlanid, tap, id FROM sensors WHERE status > 0 AND NOT status IN (3, 6)";
+$res = dbquery($sql);
+
+while (@row = $res->fetchrow_array) {
+    $arp = $row[0];
+    $dhcp = $row[1];
+    $ipv6 = $row[2];
+    $protos = $row[3];
+    $keyname = $row[4];
+    $vlanid = $row[5];
+    $tap = $row[6];
+    $sid = $row[7];
+
+    if ($c_enable_pof == 1) {
+        $chk = `ps -ef | grep p0f | grep -v grep | grep " $tap " | wc -l`;
+        chomp($chk);
+        print "POF check for $tap: $chk\n";
+        if ($chk == 0) {
+            system "p0f -d -i $tap -o /dev/null";
+        }
+    }
+    if ($c_ethernet_module == 1) {
+        # Check for ethernet modules
+        if ($arp == 1 || $dhcp == 1 || $ipv6 == 1 || $protos == 1) {
+            $chk = `ps -ef | grep detectarp | grep -v grep | grep " $tap " | wc -l`;
+            chomp($chk);
+            if ($chk == 0) {
+                system("$c_surfidsdir/scripts/detectarp.pl $tap $sid &");
+            }
+        }
+    }
+}
